da1524bec2009d272848d9f6776eb9f8e14eedf6
ignacio <nachomattos78@gmail.com>
2019-10-04 16:20:29 +0100

    finished week2 exercises

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,26 +1,41 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  return person.city === 'Manchester' ? true : false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  let buses = 0;
+  for (let i = 0; i < people; i++){
+    if (i % 40 === 0){
+     buses ++  
+  } 
+  }
+  return buses 
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  let count = 0;
+  arr.forEach((val)=> val === "sheep" ? count++ : "No sheeps in the array :(");
+  return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  let postCode = person.address.postCode.charAt(0)
+  let city = person.address.city
+  return postCode === "M" && city === "Manchester" ? true : false
+  
 }
 
 module.exports = {
--- test/week2.test.js
+++ test/week2.test.js
@@ -1,3 +1,4 @@
+
 const {
   getFillings,
   isFromManchester,
