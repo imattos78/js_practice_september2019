921bbb6cdfe4bdc6e2c2a7d43366d734c2e1c270
ignacio <nachomattos78@gmail.com>
2019-10-13 20:44:12 +0100

    week3 completed

--- challenges/week3.js
+++ challenges/week3.js
@@ -1,28 +1,38 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  let newArr = []
+  nums.map((num) => newArr.push(Math.pow(num, 2)))
+  return newArr;
 }
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+  let arr = words.slice(1)
+  let result = []
+  arr.map((w) => result.push(w.charAt(0).toUpperCase() + w.slice(1)))
+
+  return words.length > 1 ? `${words[0]}${result.join('')}` : words.toString()
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  let result = 0
+  people.forEach((sub) => result += sub.subjects.length)
+  return result
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+  const result = menu.map((arr) => Object.values(arr.ingredients)).some((i) => i.includes(ingredient))
+  return result
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+  const duplNumb = arr1.filter((num) => arr2.includes(num)).filter((num, index, arr) => arr.indexOf(num) === index).sort();
+  return duplNumb;
 }
 
 module.exports = {
